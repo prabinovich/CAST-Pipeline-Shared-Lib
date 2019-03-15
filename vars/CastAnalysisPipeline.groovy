2bdf497ca07e4f3b23597105af87f6a052f8c851
prabinovich <pavel.rabinovich@gmail.com>
2019-03-15 12:39:43 -0400

    new project

---
+++ vars/CastAnalysisPipeline.groovy
@@ -0,0 +1,69 @@
+// vars/buildPlugin.groovy
+def call (Map config){
+
+	node ('master') {
+	    stage ('CAST-Get Code') {
+	        dir('CAST-Scripts') {
+	           git branch: 'master', credentialsId: 'Github-prabinovich', url: 'https://github.com/prabinovich/CAST-Jenkins-Pipeline.git'
+	        }
+	        dir('Webstore') {
+	           git credentialsId: 'Github-prabinovich', url: 'https://github.com/prabinovich/WebStore.git'
+	        } 
+	    }
+	    
+	    stage ('CAST-Assessment Model') {
+	    	try {
+	        	echo '-- Enable Assessment Model --'
+	        	bat '"%WORKSPACE%\\CAST-Scripts\\CLI-Scripts\\CMS_ImportAssessmentModel.bat" "profile=sandbox838" "app=Webstore" "filepath=%WORKSPACE%\\CAST-Scripts\\QualityModels\\CAST 8.3.8 Assessment Model - Standard.pmx"'
+	    	}
+	    	catch (err) {
+	    		echo '*** Assessment model import failed ***'
+	    		echo err
+	    		echo '**************************************'
+	    	}
+	    }
+	    
+	    stage ('CAST-Packaging') {
+	        echo '-- Packaging and Delivery of Source Code --'
+	        bat '"%WORKSPACE%\\CAST-Scripts\\CLI-Scripts\\CMS_AutomateDelivery.bat" "profile=sandbox838" "app=Webstore" "fromVersion=Baseline" "version=version %BUILD_NUMBER%"'
+	    } 
+	    
+	    stage ('CAST-Analysis') {
+	        echo '-- Analyze Application --'
+	        bat '"%WORKSPACE%\\CAST-Scripts\\CLI-Scripts\\CMS_Analyze.bat" "profile=sandbox838" "app=Webstore"'
+	    }
+	    
+	    stage ('CAST-Snapshot') {
+	        echo '-- Generate Snapshot --'
+	        bat '"%WORKSPACE%\\CAST-Scripts\\CLI-Scripts\\CMS_GenerateSnapshot.bat" "profile=sandbox838" "app=Webstore" "version=version %BUILD_NUMBER%"'
+	    }
+	    
+	    stage('CAST-Publish Results'){
+	        echo "-- Consolidate Snapshot --"        
+	        withCredentials([usernamePassword(credentialsId: 'CAST-DB-Keys', passwordVariable: 'PWD1', usernameVariable: 'USR1')]) {
+	            bat '"%WORKSPACE%\\CAST-Scripts\\CLI-Scripts\\AAD_ConsolidateSnapshot.bat" "measure=sandbox838_measure" "central=sandbox838_central" "password=%PWD1%"'
+	        }
+	        
+	        withCredentials([usernamePassword(credentialsId: 'CAST-Dashboard-Keys', passwordVariable: 'PWD1', usernameVariable: 'USR1')]) {
+	            bat "curl.exe -u ${USR1}:${PWD1} -H \"Accept: application/json\" http://localhost:8080/CAST-Health-Engineering-838/rest/server/reload"
+	        }
+	    }
+	    
+	    stage('CAST-Publish HTML Report'){
+	 		echo "-- Create CAST Report in Jenkins --"    
+	    	dir('CAST-Report') {
+	    		withCredentials([usernamePassword(credentialsId: 'CAST-Dashboard-Keys', passwordVariable: 'PWD1', usernameVariable: 'USR1')]) {
+	    			bat 'python "%WORKSPACE%\\CAST-Scripts\\RestAPI\\CAST-Results-Report.py" --connection=http://localhost:8080/CAST-Health-Engineering-838/rest --username=%USR1% --password=%PWD1% --appname=Webstore'
+	    		}
+	    	}
+	    		publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'CAST-Report', reportFiles: 'index.html', reportName: 'CAST Analysis Report', reportTitles: ''])
+	    }
+	    
+	    stage('CAST-Check Success'){
+	    	echo "-- Quality Gate - Check Analysis Results --"
+	    	withCredentials([usernamePassword(credentialsId: 'CAST-Dashboard-Keys', passwordVariable: 'PWD1', usernameVariable: 'USR1')]) {
+	    		bat 'python "%WORKSPACE%\\CAST-Scripts\\RestAPI\\CAST-Check-Rule.py" --connection=http://localhost:8080/CAST-Health-Engineering-838/rest --username=%USR1% --password=%PWD1% --appname=Webstore --ruleid=7742'
+	    	}
+	    }
+	}
+}
